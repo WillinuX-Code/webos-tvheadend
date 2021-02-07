83f21f44c81118f55b98793af892be007f93c353
Jens Willhardt <jens.willhardt@gmail.com>
2021-02-07 14:00:49 +0100

    [Recordings] #14 initial view

---
+++ src/components/RecordingList.tsx
@@ -0,0 +1,355 @@
+import React, { useContext, useEffect, useRef } from 'react';
+import Rect from '../models/Rect';
+import CanvasUtils from '../utils/CanvasUtils';
+import '../styles/app.css';
+import Recording from '../models/Recording';
+import AppContext from '../AppContext';
+
+const VERTICAL_SCROLL_TOP_PADDING_ITEM = 5;
+const IS_DEBUG = false;
+
+const RecordingList = (props: { unmount: () => void; recordings: Recording[] }) => {
+    const { setCurrentRecordingPosition } = useContext(AppContext);
+
+    const canvas = useRef<HTMLCanvasElement>(null);
+    const listWrapper = useRef<HTMLDivElement>(null);
+    const scrollAnimationId = useRef(0);
+    const scrollY = useRef(0);
+    const recordPosition = useRef(0);
+
+    const mChannelLayoutTextSize = 32;
+    const mChannelLayoutEventTextSize = 26;
+    const mChannelLayoutNumberTextSize = 38;
+    const mChannelLayoutTextColor = '#cccccc';
+    const mChannelLayoutTitleTextColor = '#969696';
+    const mChannelLayoutMargin = 3;
+    const mChannelLayoutPadding = 7;
+    const mChannelLayoutHeight = 90;
+    const mChannelLayoutWidth = 900;
+    const mChannelLayoutBackgroundFocus = 'rgba(65,182,230,1)';
+
+    const getTopFrom = (position: number) => {
+        const y = position * mChannelLayoutHeight; //+ this.mChannelLayoutMargin;
+        return y - scrollY.current;
+    };
+
+    const scrollToChannelPosition = (channelPosition: number, withAnimation: boolean) => {
+        // start scrolling after padding position top
+        if (channelPosition < VERTICAL_SCROLL_TOP_PADDING_ITEM) {
+            scrollY.current = 0;
+            updateCanvas();
+            return;
+        }
+
+        // stop scrolling before top padding position
+        const maxPosition = props.recordings.length - VERTICAL_SCROLL_TOP_PADDING_ITEM;
+        if (channelPosition >= maxPosition) {
+            // fix scroll to channel in case it is within bottom padding
+            if (scrollY.current === 0) {
+                scrollY.current = mChannelLayoutHeight * (maxPosition - VERTICAL_SCROLL_TOP_PADDING_ITEM);
+            }
+            updateCanvas();
+            return;
+        }
+
+        // scroll to channel position
+        const scrollTarget = mChannelLayoutHeight * (channelPosition - VERTICAL_SCROLL_TOP_PADDING_ITEM);
+        if (!withAnimation) {
+            scrollY.current = scrollTarget;
+            updateCanvas();
+            return;
+        }
+
+        const scrollDistance = scrollTarget - scrollY.current;
+        const scrollDelta = scrollDistance / (mChannelLayoutHeight / 5);
+        // stop existing animation if we have a new request
+        cancelAnimationFrame(scrollAnimationId.current);
+        scrollAnimationId.current = requestAnimationFrame(() => {
+            animateScroll(scrollDelta, scrollTarget);
+        });
+    };
+
+    const animateScroll = (scrollDelta: number, scrollTarget: number) => {
+        if (scrollDelta < 0 && scrollY.current <= scrollTarget) {
+            //this.scrollY = scrollTarget;
+            cancelAnimationFrame(scrollAnimationId.current);
+            return;
+        }
+        if (scrollDelta > 0 && scrollY.current >= scrollTarget) {
+            //this.scrollY = scrollTarget;
+            cancelAnimationFrame(scrollAnimationId.current);
+            return;
+        }
+        //console.log("scrolldelta=%d, scrolltarget=%d, scrollY=%d", scrollDelta, scrollTarget, this.scrollY);
+        scrollY.current = scrollY.current + scrollDelta;
+        scrollAnimationId.current = requestAnimationFrame(() => {
+            animateScroll(scrollDelta, scrollTarget);
+        });
+        updateCanvas();
+    };
+
+    const drawChannelListItems = (canvas: CanvasRenderingContext2D) => {
+        // Background
+        const drawingRect = new Rect();
+        drawingRect.left = 0;
+        drawingRect.top = 0;
+        drawingRect.right = drawingRect.left + mChannelLayoutWidth;
+        drawingRect.bottom = drawingRect.top + getHeight();
+        canvas.globalAlpha = 1.0;
+        // put stroke color to transparent
+        //canvas.strokeStyle = "transparent";
+        canvas.strokeStyle = 'gradient';
+        //mPaint.setColor(mChannelLayoutBackground);
+        // canvas.fillStyle = this.mChannelLayoutBackground;
+        // Create gradient
+        const grd = canvas.createLinearGradient(
+            drawingRect.bottom,
+            drawingRect.top,
+            drawingRect.bottom,
+            drawingRect.bottom
+        );
+        // Important bit here is to use rgba()
+        grd.addColorStop(0, 'rgba(11, 39, 58, 0.7)');
+        grd.addColorStop(0.2, 'rgba(35, 64, 84, 0.9)');
+        grd.addColorStop(0.8, 'rgba(35, 64, 84, 0.9)');
+        grd.addColorStop(1, 'rgba(11, 39, 58, 0.7)');
+
+        // Fill with gradient
+        canvas.fillStyle = grd;
+        canvas.fillRect(drawingRect.left, drawingRect.top, drawingRect.width, drawingRect.height);
+
+        const firstPos = getFirstVisibleChannelPosition();
+        const lastPos = getLastVisibleChannelPosition();
+
+        //console.log("Channel: First: " + firstPos + " Last: " + lastPos);
+        //let transparentTop = firstPos + 3;
+        //let transparentBottom = lastPos - 3;
+        canvas.globalAlpha = 1.0;
+        for (let pos = firstPos; pos < lastPos; pos++) {
+            // if (pos <= transparentTop) {
+            //     canvas.globalAlpha += 0.25;
+            // } else if (pos >= transparentBottom) {
+            //     canvas.globalAlpha -= 0.25;
+            // } else {
+            //     canvas.globalAlpha = 1;
+            // }
+            drawChannelItem(canvas, pos);
+        }
+    };
+
+    const drawChannelItem = (canvas: CanvasRenderingContext2D, position: number) => {
+        const isSelectedChannel = position === recordPosition.current;
+        const channel = props.recordings[position];
+        const drawingRect = new Rect();
+
+        // should not happen, but better check it
+        if (!channel) return;
+
+        drawingRect.left = 0;
+        drawingRect.top = getTopFrom(position);
+        drawingRect.right = mChannelLayoutWidth;
+        drawingRect.bottom = drawingRect.top + mChannelLayoutHeight;
+        drawDebugRect(canvas, drawingRect);
+
+        // highlight selected channel
+        if (isSelectedChannel) {
+            canvas.fillStyle = mChannelLayoutBackgroundFocus;
+            canvas.fillRect(drawingRect.left, drawingRect.top, drawingRect.width, drawingRect.height);
+        }
+
+        // channel number
+        CanvasUtils.writeText(canvas, channel.getChannelID().toString(), drawingRect.left + 70, drawingRect.middle, {
+            fontSize: mChannelLayoutNumberTextSize,
+            textAlign: 'right',
+            fillStyle: mChannelLayoutTextColor,
+            isBold: true
+        });
+
+        // channel line
+        const channelIconWidth = mChannelLayoutHeight * 1.3;
+        const channelNameWidth = mChannelLayoutWidth - channelIconWidth - 90;
+
+        // channel name
+        CanvasUtils.writeText(
+            canvas,
+            channel.getName(),
+            drawingRect.left + 90,
+            drawingRect.top + mChannelLayoutHeight * 0.33,
+            {
+                fontSize: mChannelLayoutTextSize,
+                fillStyle: mChannelLayoutTextColor,
+                isBold: true,
+                maxWidth: channelNameWidth
+            }
+        );
+    };
+
+    const drawDebugRect = (canvas: CanvasRenderingContext2D, drawingRect: Rect) => {
+        if (IS_DEBUG) {
+            canvas.strokeStyle = '#FF0000';
+            canvas.strokeRect(drawingRect.left, drawingRect.top, drawingRect.width, drawingRect.height);
+        }
+    };
+
+    /**
+     * get first visible channel position
+     */
+    const getFirstVisibleChannelPosition = () => {
+        const y = scrollY.current;
+        let position = Math.floor(y / mChannelLayoutHeight);
+
+        if (position < 0) {
+            position = 0;
+        }
+        //console.log("First visible item: ", position);
+        return position;
+    };
+
+    const getLastVisibleChannelPosition = () => {
+        const y = scrollY.current;
+        const screenHeight = getHeight();
+        let position = Math.floor((y + screenHeight) / mChannelLayoutHeight);
+
+        const channelCount = props.recordings.length;
+        // this will fade the bottom channel in while scrolling
+        if (position < channelCount) {
+            position += 1;
+        }
+        // this is the max channel available
+        if (position > channelCount) {
+            position = channelCount;
+        }
+        //console.log("Last visible item: ", position);
+        return position;
+    };
+
+    const recalculateAndRedraw = (withAnimation: boolean) => {
+        if (props.recordings !== null && props.recordings.length > 0) {
+            // calculateMaxVerticalScroll();
+            scrollToChannelPosition(recordPosition.current, withAnimation);
+        }
+    };
+
+    const getWidth = () => {
+        return mChannelLayoutWidth;
+    };
+
+    const getHeight = () => {
+        return window.innerHeight;
+    };
+
+    const focus = () => {
+        listWrapper.current?.focus();
+    };
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        const keyCode = event.keyCode;
+
+        switch (keyCode) {
+            case 33: // programm up
+            case 38: // arrow up
+                event.stopPropagation();
+                scrollUp();
+                break;
+            case 34: // programm down
+            case 40: // arrow down
+                event.stopPropagation();
+                scrollDown();
+                break;
+            case 404: // TODO yellow button + back button
+            case 67: // keyboard 'c'
+            case 461: // back button
+                event.stopPropagation();
+                props.unmount();
+                break;
+            case 13: // ok button -> switch to focused channel
+                event.stopPropagation();
+                setCurrentRecordingPosition(recordPosition.current);
+                props.unmount();
+                break;
+            default:
+                console.log('RecordingList-keyPressed:', keyCode);
+        }
+
+        // pass unhandled events to parent
+        if (!event.isPropagationStopped) return event;
+    };
+
+    const handleScrollWheel = (event: React.WheelEvent<HTMLDivElement>) => {
+        event.deltaY < 0 ? scrollUp() : scrollDown();
+        focus();
+    };
+
+    const handleClick = () => {
+        setChannelPosition(recordPosition.current);
+        props.unmount();
+    };
+
+    const scrollUp = () => {
+        // if we reached 0 we scroll to end of list
+        if (recordPosition.current === 0) {
+            setChannelPosition(props.recordings.length - 1);
+        } else {
+            // channel down
+            setChannelPosition(recordPosition.current - 1);
+        }
+    };
+
+    const scrollDown = () => {
+        // when channel position increased channelcount we scroll to beginning
+        if (recordPosition.current === props.recordings.length - 1) {
+            setChannelPosition(0);
+        } else {
+            // channel up
+            setChannelPosition(recordPosition.current + 1);
+        }
+    };
+
+    const updateCanvas = () => {
+        if (canvas.current) {
+            const ctx = canvas.current.getContext('2d');
+            // clear
+            ctx && ctx.clearRect(0, 0, getWidth(), getHeight());
+
+            // draw child elements
+            ctx && onDraw(ctx);
+        }
+    };
+
+    const onDraw = (canvas: CanvasRenderingContext2D) => {
+        if (props.recordings && props.recordings.length > 0) {
+            drawChannelListItems(canvas);
+        }
+    };
+
+    const setChannelPosition = (channelPos: number) => {
+        recordPosition.current = channelPos;
+        scrollToChannelPosition(channelPos, true);
+    };
+
+    useEffect(() => {
+        recalculateAndRedraw(false);
+        focus();
+
+        return () => {
+            // stop animation when unmounting
+            cancelAnimationFrame(scrollAnimationId.current);
+        };
+    }, []);
+
+    return (
+        <div
+            id="channellist-wrapper"
+            ref={listWrapper}
+            tabIndex={-1}
+            onKeyDown={handleKeyPress}
+            onWheel={handleScrollWheel}
+            onClick={handleClick}
+            className="channelList"
+        >
+            <canvas ref={canvas} width={getWidth()} height={getHeight()} style={{ display: 'block' }} />
+        </div>
+    );
+};
+
+export default RecordingList;

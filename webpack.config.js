e9e720b9409306f972e0bf888712c8d403a964b0
jwillhardt <willinuxcode@gmail.com>
2023-03-15 23:41:27 +0100

    #21 switch to webpack to solve transpile and polyfill errors

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
+                //exclude: /node_modules/,
                 use: ['babel-loader']
             },
             {
